2398c6e77390bb20234502841c243fe9ed6e295e
Kyle Souther-Bruno <ksoutherbruno@gmail.com>
2019-02-13 12:12:31 -0600

    added header and CSS to webpage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-Header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
            <code>src/App.js</code> and save to reload.
@@ -20,10 +21,11 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
